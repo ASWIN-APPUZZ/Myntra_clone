042dcd86e33ea8c66adcad327e327d38f320faa9
aswin <aswin0484@gmail.com>
2024-01-29 16:46:13 +0530

    npm

--- routes/product_details.routes.js
+++ routes/product_details.routes.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const middleware = require('../middleware');
-const router = express.Router(); 
+const router = express.Router();
 const multer = require('multer');
-const csvFile =require("csvtojson");
-
+const csvFile = require("csvtojson");
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "./uploads");
-    },
+    destination: (req, file, cb) => { cb(null, "./uploads"); },
     filename: (req, file, cb) => {
-        cb(null, file.originalname); // Corrected typo
+        cb(null, file.originalname);
     }
 });
-
 const upload = multer({ storage });
-
 router.post("/uploadAll", upload.single("csvFile"), async (req, res) => {
     const jsonArray = await csvFile().fromFile(req.file.path);
     res.json(jsonArray);
 });
-
 module.exports = router;
